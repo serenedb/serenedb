369ddca30bc5a654858a6ee8c4d8c45524e108e0
Pablo Pudgino <pavel.ivanov@serenedb.com>
2026-01-02 04:48:04 +0100

    fix:

--- server/pg/sql_resolver.cpp
+++ server/pg/sql_resolver.cpp
@@ -32,10 +32,6 @@
 
 namespace sdb::pg {
 
-void ResolveObjects(ObjectId database, std::span<const std::string> search_path,
-                    Objects& objects, Disallowed& disallowed,
-                    const Objects& query);
-
 namespace {
 
 void ResolveInformationSchema(ObjectId database, std::string_view relation,
@@ -47,30 +43,23 @@ void ResolveInformationSchema(ObjectId database, std::string_view relation,
   // TODO(codeworse): add views and functions from information_schema
 }
 
-enum class ObjectType : uint8_t {
-  Function = 0,
-  Relation = 1  // view, table
-};
+void ResolveFunctions(ObjectId database,
+                      std::span<const std::string> search_path,
+                      Objects& objects, Disallowed& disallowed,
+                      const Objects& query);
 
-void ResolveObject(ObjectId database, ObjectType type,
-                   std::span<const std::string> search_path, Objects& objects,
-                   Disallowed& disallowed, const Objects::ObjectName& name,
-                   Objects::ObjectData& data) {
+void ResolveFunction(ObjectId database,
+                     std::span<const std::string> search_path, Objects& objects,
+                     Disallowed& disallowed, const Objects::ObjectName& name,
+                     Objects::ObjectData& data) {
   if (data.object) {
     return;
   }
 
-  if (type == ObjectType::Relation) {
-    // system tables
-    if (const auto* table = pg::GetTable(name.relation)) {
-      data.object = table->CreateSnapshot(database);
-      return;
-    }
-  } else if (type == ObjectType::Function) {
-    if (const auto func = pg::GetFunction(name.relation)) {
-      data.object = std::move(func);
-      return;
-    }
+  // system functions
+  if (const auto func = pg::GetFunction(name.relation)) {
+    data.object = std::move(func);
+    return;
   }
 
   if (name.schema == StaticStrings::kInformationSchema) {
@@ -78,24 +67,6 @@ void ResolveObject(ObjectId database, ObjectType type,
     // (except the case it is in the search path)
     ResolveInformationSchema(database, name.relation, data);
     if (data.object) {
-      return;
-    }
-  }
-
-  auto resolve_subquery = [&] {
-    bool changed = disallowed.emplace(name).second;
-    SDB_ASSERT(changed);
-    auto state = basics::downCast<SqlQueryView>(*data.object).GetState();
-    ResolveQueryView(database, search_path, objects, disallowed,
-                     state->objects);
-    changed = disallowed.erase(name) != 0;
-    SDB_ASSERT(changed);
-  };
-
-  if (type == ObjectType::Relation) {
-    if (const auto view = pg::GetView(name.relation)) {
-      data.object = std::move(view);
-      resolve_subquery();
       return;
     }
   }
@@ -111,14 +82,7 @@ void ResolveObject(ObjectId database, ObjectType type,
     auto& instance = SerenedServer::Instance();
     auto& catalog = instance.getFeature<catalog::CatalogFeature>().Global();
     auto snapshot = catalog.GetSnapshot();
-    data.object = [&] -> std::shared_ptr<catalog::SchemaObject> {
-      switch (type) {
-        case ObjectType::Relation:
-          return snapshot->GetRelation(database, schema, name.relation);
-        case ObjectType::Function:
-          return snapshot->GetFunction(database, schema, name.relation);
-      }
-    }();
+    data.object = snapshot->GetFunction(database, schema, name.relation);
   };
 
   if (!name.schema.empty()) {
@@ -133,56 +97,116 @@ void ResolveObject(ObjectId database, ObjectType type,
   }
 
   if (!data.object) {
-    const std::string object_name = [&] {
-      switch (type) {
-        case ObjectType::Relation:
-          return "relation";
-        case ObjectType::Function:
-          return "function";
-      }
-    }();
-    SDB_THROW(ERROR_SERVER_DATA_SOURCE_NOT_FOUND, object_name, " \"",
+    SDB_THROW(ERROR_SERVER_DATA_SOURCE_NOT_FOUND, "function \"",
               name.FullName(), "\" does not exist");
   }
 
-  if (type == ObjectType::Relation) {
-    if (data.object->GetType() == catalog::ObjectType::Table) {
-      auto table = basics::downCast<catalog::Table>(*data.object);
-      for (const auto& column : table.Columns()) {
-        if (const auto& default_value = column.expr) {
-          const auto& default_value_objects = default_value->GetObjects();
-          ResolveObjects(database, search_path, objects, disallowed,
-                         default_value_objects);
-        }
-      }
-    } else if (data.object->GetType() == catalog::ObjectType::View) {
-      resolve_subquery();
-    }
-  } else if (type == ObjectType::Function) {
-    SDB_ASSERT(data.object->GetType() == catalog::ObjectType::Function);
-    auto& func = basics::downCast<catalog::Function>(*data.object);
-    if (func.Options().language == catalog::FunctionLanguage::SQL) {
-      bool changed = disallowed.emplace(name).second;
-      SDB_ASSERT(changed);
-      ResolveSqlFunction(database, search_path, objects, disallowed,
-                         func.SqlFunction().GetObjects());
-      changed = disallowed.erase(name) != 0;
-      SDB_ASSERT(changed);
-    }
+  SDB_ASSERT(data.object->GetType() == catalog::ObjectType::Function);
+  auto& func = basics::downCast<catalog::Function>(*data.object);
+  if (func.Options().language == catalog::FunctionLanguage::SQL) {
+    bool changed = disallowed.emplace(name).second;
+    SDB_ASSERT(changed);
+    ResolveSqlFunction(database, search_path, objects, disallowed,
+                       func.SqlFunction().GetObjects());
+    changed = disallowed.erase(name) != 0;
+    SDB_ASSERT(changed);
   }
 }
 
-}  // namespace
+// view, table
+void ResolveRelation(ObjectId database,
+                     std::span<const std::string> search_path, Objects& objects,
+                     Disallowed& disallowed, const Objects::ObjectName& name,
+                     Objects::ObjectData& data) {
+  if (data.object) {
+    return;
+  }
+
+  // system tables
+  if (const auto* table = pg::GetTable(name.relation)) {
+    data.object = table->CreateSnapshot(database);
+    return;
+  }
+
+  if (name.schema == StaticStrings::kInformationSchema) {
+    // information_schema must be explicitly defined
+    // (except the case it is in the search path)
+    ResolveInformationSchema(database, name.relation, data);
+    if (data.object) {
+      return;
+    }
+  }
+
+  auto resolve_view = [&] {
+    bool changed = disallowed.emplace(name).second;
+    SDB_ASSERT(changed);
+    auto state = basics::downCast<SqlQueryView>(*data.object).GetState();
+    ResolveQueryView(database, search_path, objects, disallowed,
+                     state->objects);
+    changed = disallowed.erase(name) != 0;
+    SDB_ASSERT(changed);
+  };
+
+  // system views
+  if (const auto view = pg::GetView(name.relation)) {
+    data.object = std::move(view);
+    resolve_view();
+    return;
+  }
+
+  auto resolve_object = [&](std::string_view schema) {
+    SDB_ASSERT(!data.object);
+    if (schema == StaticStrings::kInformationSchema) {
+      // In case information_schema is in the search path
+      ResolveInformationSchema(database, name.relation, data);
+      return;
+    }
+
+    auto& instance = SerenedServer::Instance();
+    auto& catalog = instance.getFeature<catalog::CatalogFeature>().Global();
+    auto snapshot = catalog.GetSnapshot();
+    data.object = snapshot->GetRelation(database, schema, name.relation);
+  };
+
+  if (!name.schema.empty()) {
+    resolve_object(name.schema);
+  } else {
+    for (const auto& schema : search_path) {
+      resolve_object(schema);
+      if (data.object) {
+        break;
+      }
+    }
+  }
+
+  if (!data.object) {
+    SDB_THROW(ERROR_SERVER_DATA_SOURCE_NOT_FOUND, "relation \"",
+              name.FullName(), "\" does not exist");
+  }
+
+  if (data.object->GetType() == catalog::ObjectType::Table) {
+    auto table = basics::downCast<catalog::Table>(*data.object);
+    for (const auto& column : table.Columns()) {
+      if (const auto& default_value = column.expr) {
+        const auto& default_value_objects = default_value->GetObjects();
+        SDB_ASSERT(default_value_objects.getRelations().empty());
+        ResolveFunctions(database, search_path, objects, disallowed,
+                         default_value_objects);
+      }
+    }
+  } else if (data.object->GetType() == catalog::ObjectType::View) {
+    resolve_view();
+  }
+}
 
 void ResolveRelations(ObjectId database,
                       std::span<const std::string> search_path,
                       Objects& objects, Disallowed& disallowed,
                       const Objects& query) {
-  for (auto& [name, old_data] : objects.getRelations()) {
+  for (auto& [name, old_data] : query.getRelations()) {
     auto& new_data = objects.ensureRelation(name.schema, name.relation);
-    new_data = std::move(old_data);
-    ResolveObject(database, ObjectType::Relation, search_path, objects,
-                  disallowed, name, new_data);
+    new_data = old_data;
+    ResolveRelation(database, search_path, objects, disallowed, name, new_data);
   }
 }
 
@@ -190,27 +214,33 @@ void ResolveFunctions(ObjectId database,
                       std::span<const std::string> search_path,
                       Objects& objects, Disallowed& disallowed,
                       const Objects& query) {
-  auto functions = std::move(objects.getFunctions());
-  for (auto& [name, old_data] : functions) {
+  for (auto& [name, old_data] : query.getFunctions()) {
     auto& new_data = objects.ensureFunction(name.schema, name.relation);
-    new_data = std::move(old_data);
-    ResolveObject(database, ObjectType::Function, search_path, objects,
-                  disallowed, name, new_data);
+    new_data = old_data;
+    ResolveFunction(database, search_path, objects, disallowed, name, new_data);
   }
 }
 
-void ResolveObjects(ObjectId database, std::span<const std::string> search_path,
-                    Objects& objects, Disallowed& disallowed,
-                    const Objects& query) {
-  ResolveRelations(database, search_path, objects, disallowed, query);
-  ResolveFunctions(database, search_path, objects, disallowed, query);
-}
+}  // namespace
 
 void Resolve(ObjectId database, Objects& objects, const Config& config) {
   SDB_ASSERT(!ServerState::instance()->IsDBServer());
   Disallowed dummy;
   auto search_path = config.Get<VariableType::PgSearchPath>("search_path");
-  ResolveObjects(database, search_path, objects, dummy, objects);
+
+  auto functions = std::move(objects.getFunctions());
+  for (auto& [name, old_data] : functions) {
+    auto& new_data = objects.ensureFunction(name.schema, name.relation);
+    new_data = std::move(old_data);
+    ResolveFunction(database, search_path, objects, dummy, name, new_data);
+  }
+
+  auto relations = std::move(objects.getRelations());
+  for (auto& [name, old_data] : relations) {
+    auto& new_data = objects.ensureRelation(name.schema, name.relation);
+    new_data = std::move(old_data);
+    ResolveRelation(database, search_path, objects, dummy, name, new_data);
+  }
 }
 
 void ResolveQueryView(ObjectId database,
@@ -224,27 +254,16 @@ void ResolveQueryView(ObjectId database,
     }
     auto& new_data = objects.ensureRelation(name.schema, name.relation);
     new_data = old_data;
-    ResolveObject(database, ObjectType::Relation, search_path, objects,
-                  disallowed, name, new_data);
+    ResolveRelation(database, search_path, objects, disallowed, name, new_data);
   }
-  for (const auto& [name, old_data] : query.getFunctions()) {
-    auto& new_data = objects.ensureFunction(name.schema, name.relation);
-    new_data = old_data;
-    ResolveObject(database, ObjectType::Function, search_path, objects,
-                  disallowed, name, new_data);
-  }
+  ResolveFunctions(database, search_path, objects, disallowed, query);
 }
 
 void ResolveSqlFunction(ObjectId database,
                         std::span<const std::string> search_path,
                         Objects& objects, Disallowed& disallowed,
                         const Objects& query) {
-  for (const auto& [name, old_data] : query.getRelations()) {
-    auto& new_data = objects.ensureRelation(name.schema, name.relation);
-    new_data = old_data;
-    ResolveObject(database, ObjectType::Relation, search_path, objects,
-                  disallowed, name, new_data);
-  }
+  ResolveRelations(database, search_path, objects, disallowed, query);
   for (const auto& [name, old_data] : query.getFunctions()) {
     if (disallowed.contains(name)) {
       SDB_THROW(ERROR_BAD_PARAMETER,
@@ -252,8 +271,7 @@ void ResolveSqlFunction(ObjectId database,
     }
     auto& new_data = objects.ensureFunction(name.schema, name.relation);
     new_data = old_data;
-    ResolveObject(database, ObjectType::Function, search_path, objects,
-                  disallowed, name, new_data);
+    ResolveFunction(database, search_path, objects, disallowed, name, new_data);
   }
 }
 

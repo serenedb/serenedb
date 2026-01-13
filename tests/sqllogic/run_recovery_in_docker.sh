9a3bbd100b13bccb576029ed855240313ef2a849
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2026-01-13 16:09:40 +0100

    Single file

---
+++ tests/sqllogic/run_recovery_in_docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if test -z "$SQLLOGIC_DIR"; then
+  export SQLLOGIC_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+fi
+
+if ! test -f "$SQLLOGIC_DIR/run_recovery_in_docker.sh"; then
+    echo "SQLLOGIC_DIR is undefined or invalid"
+    exit 255
+fi
+
+# In case of running locally
+if test -z "$WORKSPACE"; then
+    export WORKSPACE=$(realpath "$SQLLOGIC_DIR/../../")
+fi
+
+if ! test -f "$WORKSPACE/docker.env"; then
+    touch "$WORKSPACE/docker.env"
+fi
+
+# Docker container assumes that sanitizers and coverage dirs are created.
+# Executable 'serened' switches to 'serenedb' user. If required dirs are not created,
+# /serenedb dir owner is 'root' and it's impossible for 'serenedb' user to create directory there.
+mkdir -p "$WORKSPACE/sanitizers"
+mkdir -p "$WORKSPACE/build/coverage"
+
+if test -z "$BUILD_IMAGE"; then
+    export BUILD_IMAGE=10.serenedb.com:5000/serenedb-build-ubuntu:latest
+fi
+
+cd $SQLLOGIC_DIR
+# can be useful to run from container: docker compose run tests bash
+docker compose -f docker-compose.recovery.yml up --attach tests --exit-code-from tests --remove-orphans
+test_exit_code=$?
+if ! test "${test_exit_code}" -eq "0"; then
+  echo "Recovery tests failed!"
+  echo "serenedb-single container log begin:"
+  docker compose logs serenedb-single
+  echo "serenedb-single container log end!"
+fi
+docker compose down --volumes
+exit "$test_exit_code"

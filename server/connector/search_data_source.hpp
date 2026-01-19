cad1d89f5995ad817acd8c381b33ca03b7572bc4
Andrei L <andrei.lobov@serenedb.com>
2026-01-19 17:03:37 +0100

    wip

---
+++ server/connector/search_data_source.hpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2025 SereneDB GmbH, Berlin, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is SereneDB GmbH, Berlin, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "basics/fwd.h"
+#include <velox/connectors/Connector.h>
+
+
+namespace sdb::connector::search {
+
+class SearchIndexSource final : public velox::connector::IndexSource {
+ public:
+  SearchIndexSource() = default;
+
+  ~SearchIndexSource() override = default;
+
+  std::shared_ptr<LookupResultIterator> lookup(
+      const LookupRequest& request) final;
+
+  std::unordered_map<std::string, RuntimeMetric> runtimeStats() final {
+    // TODO(Dronn): implement metrics
+    return {};
+  }
+};
+
+} // namespace sdb::connector::search

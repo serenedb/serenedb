7a21974a08591ef15b0b4a9ef8e06361e2dbb0cc
aksel2904 <aksel2904@gmail.com>
2026-02-13 12:19:13 +0000

    feat: added few fixes

--- libs/iresearch/include/iresearch/search/regexp_filter.cpp
+++ libs/iresearch/include/iresearch/search/regexp_filter.cpp
@@ -1,3 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2025 SereneDB GmbH, Berlin, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is SereneDB GmbH, Berlin, Germany
+////////////////////////////////////////////////////////////////////////////////
+
 #include "regexp_filter.hpp"
 
 #include "iresearch/index/index_reader.hpp"
@@ -38,13 +58,13 @@ field_visitor ByRegexp::visitor(bytes_view pattern) {
             matcher{MakeAutomatonMatcher(acceptor)} {}  //  matcher
 
         automaton acceptor;               // dfa from regexp
-        automaton_table_matcher matcher;  // matcher for пересечения с fst
+        automaton_table_matcher matcher;  // matcher for fst intersection
       };
 
       auto ctx = std::make_shared<AutomatonContext>(pattern);
 
       if (!Validate(ctx->acceptor)) {
-        return [](const SubReader&, const TermReader&, FilterVisitor&) {};
+        SDB_UNREACHABLE();
       }
 
       return [ctx = std::move(ctx)](const SubReader& segment,
@@ -73,7 +93,6 @@ Filter::Query::ptr ByRegexp::prepare(const PrepareContext& ctx,
     }
 
     case RegexpType::Complex:
-      // Сложный паттерн — нужен автомат
       break;
   }
 

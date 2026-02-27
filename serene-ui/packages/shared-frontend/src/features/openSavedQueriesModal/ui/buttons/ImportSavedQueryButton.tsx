a6a65004d4fd10f233594a37ba941854ea5c0452
iljaname <ilja-vlasko00@mail.ru>
2026-02-27 13:50:46 +0300

    limited file size to import in ui

--- serene-ui/packages/shared-frontend/src/features/openSavedQueriesModal/ui/buttons/ImportSavedQueryButton.tsx
+++ serene-ui/packages/shared-frontend/src/features/openSavedQueriesModal/ui/buttons/ImportSavedQueryButton.tsx
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 import { useSavedQueriesModal } from "../../model";
 import { toast } from "sonner";
 
+const MAX_SQL_FILE_SIZE_BYTES = 1024 * 1024;
+
 export const ImportSavedQueryButton = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [isImporting, setIsImporting] = useState(false);
@@ -22,6 +24,14 @@ export const ImportSavedQueryButton = () => {
 
         try {
             setIsImporting(true);
+
+            if (file.size > MAX_SQL_FILE_SIZE_BYTES) {
+                toast.error("Failed to import query", {
+                    description: `Selected SQL file is too large. Maximum allowed size is ${MAX_SQL_FILE_SIZE_BYTES} bytes.`,
+                });
+                return;
+            }
+
             const query = await file.text();
 
             if (!query.trim()) {

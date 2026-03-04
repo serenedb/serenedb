259964df075b74fe38237099e80f3aa112d57f87
Andrei L <andrei.lobov@serenedb.com>
2026-03-04 21:53:29 +0100

    restore defaults

--- server/catalog/inverted_index.cpp
+++ server/catalog/inverted_index.cpp
@@ -11,8 +11,8 @@ ResultOr<std::shared_ptr<IndexShard>> InvertedIndex::CreateIndexShard(
   // TODO(codeworse): parse args into InvertedIndexShardOptions
   search::InvertedIndexShardOptions options;
   options.commit_interval_ms = 1000;
-  options.consolidation_interval_ms = 10000;
-  options.cleanup_interval_step = 3;
+  options.consolidation_interval_ms = 1000;
+  options.cleanup_interval_step = 1;
   auto inverted_index_shard =
     search::InvertedIndexShard::Create(*this, options, is_new);
   return inverted_index_shard;

52655d35893f6438128c57ef8a570098fc73783d
Alexander Malandin <malandin@serenedb.com>
2026-03-22 02:56:52 +0100

    push to docker hub instead of private registry

--- serene-ui/scripts/build_docker.sh
+++ serene-ui/scripts/build_docker.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+set -eo pipefail
 
-[ -z "$PUSH_IMAGE_2_REGISTRY" ] && exit 1 # PUSH_IMAGE_2_REGISTRY="false"
-[ -z "$DOCKER_TAG" ] && exit 1            # DOCKER_TAG="latest"
+[ -z "$IMAGE" ] && exit 1              # IMAGE="serenedb/serene-ui:1.2.3"
+[ -z "$DOCKER_REGISTRY" ] && exit 1   # DOCKER_REGISTRY="serenedb"
+[ -z "$PUSH_IMAGE_TO_REGISTRY" ] && exit 1 # PUSH_IMAGE_TO_REGISTRY="false"
 
-imagename=registry.serenedb.com:5000/serene-ui:${DOCKER_TAG}
-
-if [ "$PUSH_IMAGE_2_REGISTRY" = true ]; then
-	docker build --pull --no-cache --tag $imagename . && docker push $imagename
-else
-	docker build --pull --no-cache --tag $imagename .
+FULL_IMAGE_NAME="${DOCKER_REGISTRY}/serene-ui"
+[ -d logs ] || mkdir logs
+docker build --pull --no-cache --tag "$IMAGE" . 2>&1 | tee -a logs/build.log
+if [ "$PUSH_IMAGE_TO_REGISTRY" = "true" ]; then
+  if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
+    if [[ "$DOCKER_REGISTRY" =~ [.:] ]]; then
+      echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" --password-stdin
+      trap "docker logout $DOCKER_REGISTRY" EXIT
+    else
+      echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+      trap "docker logout" EXIT
+    fi
+  fi
+  docker push "$IMAGE" 2>&1 | tee -a logs/build.log
+  extra_tags="$EXTRA_DOCKER_TAGS"
+  if [ "$PUSH_RELEASE" = "true" ]; then
+    extra_tags="latest $extra_tags"
+  fi
+  IFS=', ' read -ra EXTRA_TAGS <<< "$extra_tags"
+  for tag in "${EXTRA_TAGS[@]}"; do
+    [ -z "$tag" ] && continue
+    docker tag "$IMAGE" "$FULL_IMAGE_NAME:$tag"
+    docker push "$FULL_IMAGE_NAME:$tag" 2>&1 | tee -a logs/build.log
+  done
 fi

a8f7a728ceab95d8c123f969f2422bd9f9d6a0a0
pavel vedernikoff <pavel.ivanov@serenedb.com>
2026-03-31 06:39:32 +0200

    feat: Support secondary indexes

--- server/connector/data_source.hpp
+++ server/connector/data_source.hpp
@@ -32,10 +32,13 @@
 #include <numeric>
 #include <span>
 
+#include "basics/containers/bitset.hpp"
 #include "catalog/identifiers/object_id.h"
 #include "catalog/table_options.h"
+#include "connector/common.h"
 #include "connector/key_builder.hpp"
 #include "connector/rocksdb_filter.hpp"
+#include "connector/rocksdb_materializer.hpp"
 #include "rocksdb/db.h"
 #include "rocksdb/utilities/transaction.h"
 #include "rocksdb_engine_catalog/rocksdb_option_feature.h"
@@ -209,6 +212,126 @@ class RocksDBFullScanDataSource : public RocksDBBaseDataSource {
   // this case is handled in SqlAnalyzer code, such scans are replaced with
   // empty Values node.
   catalog::Column::Id _effective_column_id;
+};
+
+class ColumnCollector {
+ public:
+  static constexpr bool kHasMaterializer = false;
+
+  void Init(const velox::TypePtr& type, size_t capacity,
+            velox::memory::MemoryPool& pool) {
+    _type_kind = type->kind();
+    _vec = VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(CreateColumnVector, _type_kind,
+                                              capacity, pool);
+    _present_rows.reset(capacity);
+  }
+
+  void Fill(size_t batch_idx, size_t found_idx,
+            std::span<const rocksdb::PinnableSlice> values) {
+    _present_rows.set(batch_idx);
+    VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(FillColumnValues, _type_kind, *_vec,
+                                       found_idx, values);
+  }
+
+  velox::VectorPtr Finish(size_t found_count) {
+    SDB_ASSERT(_present_rows.count() == found_count);
+    _vec->resize(found_count);
+    return std::move(_vec);
+  }
+
+  const irs::bitset& PresentRows() const { return _present_rows; }
+
+ private:
+  template<typename T>
+  static void SetResultValue(std::string_view value, size_t idx,
+                             velox::FlatVector<T>& result) {
+    if constexpr (std::is_same_v<T, velox::StringView>) {
+      const size_t offset = value[0] == 0 ? 1 : 0;
+      result.set(
+        idx, velox::StringView(value.data() + offset, value.size() - offset));
+    } else if constexpr (std::is_same_v<T, bool>) {
+      SDB_ASSERT(value.size() == kTrueValue.size(),
+                 "unexpected value size for bool column");
+      result.set(idx, value == kTrueValue);
+    } else {
+      SDB_ASSERT(value.size() == sizeof(T),
+                 "unexpected value size for scalar column");
+      T tmp;
+      memcpy(&tmp, value.data(), sizeof(T));
+      result.set(idx, tmp);
+    }
+  }
+
+  template<velox::TypeKind Kind>
+  static velox::VectorPtr CreateColumnVector(size_t count,
+                                             velox::memory::MemoryPool& pool) {
+    using T = typename velox::TypeTraits<Kind>::NativeType;
+    return velox::BaseVector::create<velox::FlatVector<T>>(
+      velox::Type::create<Kind>(), count, &pool);
+  }
+
+  template<velox::TypeKind Kind>
+  static void FillColumnValues(velox::BaseVector& result, size_t offset,
+                               std::span<const rocksdb::PinnableSlice> values) {
+    using T = typename velox::TypeTraits<Kind>::NativeType;
+    auto& flat = static_cast<velox::FlatVector<T>&>(result);
+    for (size_t i = 0; i < values.size(); ++i) {
+      if (values[i].empty()) {
+        flat.setNull(offset + i, true);
+      } else {
+        SetResultValue(values[i].ToStringView(), offset + i, flat);
+      }
+    }
+  }
+
+  velox::TypeKind _type_kind;
+  velox::VectorPtr _vec;
+  irs::bitset _present_rows;
+};
+
+class MaterializerCollector {
+ public:
+  static constexpr bool kHasMaterializer = true;
+
+  MaterializerCollector(RocksDBMaterializer materializer,
+                        velox::memory::MemoryPool& pool)
+    : _materializer{std::move(materializer)}, _row_keys{pool} {}
+
+  void Init(const velox::TypePtr& type, size_t capacity,
+            velox::memory::MemoryPool& pool) {
+    _row_keys.reserve(capacity);
+  }
+
+  void Fill(size_t batch_idx, size_t found_idx,
+            std::span<const rocksdb::PinnableSlice> values) {
+    for (const auto& val : values) {
+      _row_keys.emplace_back(val.data(), val.size());
+    }
+  }
+
+  velox::RowVectorPtr Finish(size_t found_count) {
+    return _materializer.ReadRows(_row_keys, nullptr);
+  }
+
+ private:
+  RocksDBMaterializer _materializer;
+  primary_key::Keys _row_keys;
+};
+
+template<bool ReadYourOwnWrites>
+struct PrimaryLookupPolicy {
+  using Source =
+    std::conditional_t<ReadYourOwnWrites, rocksdb::Transaction, rocksdb::DB>;
+  using KeyBuilder = PrimaryKeyBuilder;
+  using ResultCollector = ColumnCollector;
+};
+
+template<bool ReadYourOwnWrites>
+struct SecondaryLookupPolicy {
+  using Source =
+    std::conditional_t<ReadYourOwnWrites, rocksdb::Transaction, rocksdb::DB>;
+  using KeyBuilder = SecondaryKeyBuilder;
+  using ResultCollector = MaterializerCollector;
 };
 
 template<typename Policy>

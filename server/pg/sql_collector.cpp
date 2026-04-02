7ce73a043cb147e5a964394fd9d2fa72b19f48b2
pilik <sosomaso90@gmail.com>
2026-04-02 11:09:22 +0400

    fix: Add values coerciions (#483)
    Signed-off-by: pilik <sosomaso90@gmail.com>
    Co-authored-by: Pablo Pudgino <pavel.ivanov@serenedb.com>

--- server/pg/sql_collector.cpp
+++ server/pg/sql_collector.cpp
@@ -491,9 +491,8 @@ void ObjectCollector::CollectValuesLists(const State& state,
     if (tuple_length < 0) {
       tuple_length = list_length(&tuple);
     } else if (tuple_length != list_length(&tuple)) {
-      THROW_SQL_ERROR(
-        ERR_CODE(ERRCODE_INVALID_PARAMETER_VALUE),
-        ERR_MSG("VALUES lists must have the same number of columns"));
+      THROW_SQL_ERROR(ERR_CODE(ERRCODE_SYNTAX_ERROR),
+                      ERR_MSG("VALUES lists must all be the same length"));
     }
     VisitNodes(&tuple,
                [&](const Node& expr) { CollectExprNode(state, &expr); });

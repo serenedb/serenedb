fddcaaa9c0885754b7eb009c882f5b3cd344b576
Valery Mironov <valera.mironow@gmail.com>
2026-04-05 02:26:29 +0200

    feat(CI): Add RTA for all package types, overhaul packaging and community docs (#496)
    - Add Release Test Automation (RTA) for deb, tarball, and Docker
    packages with sqllogic tests
    - Split Docker build/push into separate scripts for RTA testing before
    push
    - Overhaul debian packaging: non-root builds, systemd service, correct
    FHS paths (sbin -> bin)
    - Fix Docker image tarball extraction (usr-merge compatibility)
    - Correct glibc license files (GPL-3/LGPL-3 -> LGPL-2.1/GPL-2/LICENSES)
    - Add community docs: CONTRIBUTING.md, CODE_OF_CONDUCT.md, SECURITY.md,
    issue/PR templates
    - Normalize checkout across all workflows: explicit params, remove
    PAT_TOKEN, shallow submodules
    - Switch .gitmodules from SSH to HTTPS for external contributors
    - Add pre-commit hooks for snake_case filenames and C++ extensions
    - Remove legacy: man pages, serenedb-helper, serenedb-update-db, SysV
    init script, serene-secure-installation, serene-init-database
    
    ---------
    
    Co-authored-by: mbkkt <mbkkt@users.noreply.github.com>

--- NOTICE.md
+++ NOTICE.md
@@ -22,6 +22,10 @@ The original work is licensed under the Apache 2.0 license.
 
 Modifications and new code added are licensed under corresponding [license](LICENSE).
 
+#### GNU C Library (glibc)
+
+Release binaries are statically linked against [glibc](https://www.gnu.org/software/libc/), which is licensed under the [LGPL-2.1](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html). License files from glibc are included in the distribution under `glibc/`.
+
 #### ArangoDB
 
 Some code is based on [ArangoDB](https://github.com/arangodb/arangodb), specifically incorporating code from [commit](https://github.com/arangodb/arangodb/commit/bdac13f0edef5ff69d7d9ae5758a30072bd6d312).
--- cmake/SereneDBInstall.cmake
+++ cmake/SereneDBInstall.cmake
@@ -28,13 +28,6 @@ else()
     set(CMAKE_INSTALL_DEBINFO_DIR "${CMAKE_INSTALL_LIBDIR}/debug/")
 endif()
 
-set(CMAKE_INSTALL_SYSCONFDIR_SERENE
-    "${CMAKE_INSTALL_SYSCONFDIR}/${CMAKE_PROJECT_NAME}"
-)
-set(CMAKE_INSTALL_FULL_SYSCONFDIR_SERENE
-    "${CMAKE_INSTALL_FULL_SYSCONFDIR}/${CMAKE_PROJECT_NAME}"
-)
-
 # database directory
 set(SERENEDB_DB_DIRECTORY "${PROJECT_BINARY_DIR}/var/lib/${CMAKE_PROJECT_NAME}")
 file(MAKE_DIRECTORY ${SERENEDB_DB_DIRECTORY})
@@ -52,16 +45,13 @@ set(CMAKE_TEST_DIRECTORY "tests")
 include(InstallMacros)
 
 # install ----------------------------------------------------------------------
-if(UNIX)
-    install(
-        DIRECTORY ${SERENEDB_MAN_DIR}
-        DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}
-    )
-endif()
-
-install_readme(resources/licenses/GPL-3 GPL-3)
-install_readme(resources/licenses/LGPL-3 LGPL-3)
 install_readme(LICENSE LICENSE.txt)
+
+# glibc license files (required for static linking, see glibc LICENSES file)
+install(
+    DIRECTORY ${CMAKE_SOURCE_DIR}/resources/licenses/glibc/
+    DESTINATION ${CMAKE_INSTALL_DOCDIR}/glibc
+)
 
 ################################################################################
 ### @brief install log directory
@@ -81,65 +71,9 @@ install(
     DESTINATION ${CMAKE_INSTALL_LOCALSTATEDIR}/lib
 )
 
-################################################################################
-### @brief detect if we're on a systemd enabled system; if install unit file.
-################################################################################
+# systemd service file is installed via debian packaging
+# (packages/debian/source/common/serenedb.service)
 
-set(SYSTEMD_FOUND false)
-set(IS_SYSTEMD_INSTALL 0)
-
-if(UNIX)
-    set(SERVICE_NAME "serenedb")
-
-    # use pkgconfig for systemd detection
-    find_package(PkgConfig QUIET)
-    if(NOT PKG_CONFIG_FOUND)
-        message(STATUS "pkg-config not found - skipping systemd detection")
-    else()
-        set(SYSTEMD_UNIT_DIR "")
-        message(STATUS "detecting systemd")
-        pkg_check_modules(SYSTEMD systemd)
-
-        if(SYSTEMD_FOUND)
-            message(STATUS "-- systemd found")
-
-            # get systemd_unit_dir -- e.g /lib/systemd/system/
-            # cmake to old: pkg_get_variable(SYSTEMD_UNIT_DIR systemd systemdsystemunitdir)
-            execute_process(
-                COMMAND
-                    ${PKG_CONFIG_EXECUTABLE} systemd
-                    --variable=systemdsystemunitdir
-                OUTPUT_VARIABLE SYSTEMD_UNIT_DIR
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-            )
-            set(IS_SYSTEMD_INSTALL 1)
-
-            # set prefix
-            if(
-                CMAKE_INSTALL_PREFIX
-                AND NOT "${CMAKE_INSTALL_PREFIX}" STREQUAL "/"
-            )
-                set(SYSTEMD_UNIT_DIR
-                    "${CMAKE_INSTALL_PREFIX}/${SYSTEMD_UNIT_DIR}/"
-                )
-            endif()
-
-            # configure and install systemd service
-            configure_file(
-                ${SERENEDB_SOURCE_DIR}/packages/systemd/serenedb.service.in
-                ${PROJECT_BINARY_DIR}/serenedb.service
-                NEWLINE_STYLE UNIX
-            )
-            install(
-                FILES ${PROJECT_BINARY_DIR}/serenedb.service
-                DESTINATION ${SYSTEMD_UNIT_DIR}/
-                RENAME ${SERVICE_NAME}.service
-            )
-        else()
-            message(STATUS "-- systemd not found")
-        endif(SYSTEMD_FOUND)
-    endif(NOT PKG_CONFIG_FOUND)
-endif(UNIX)
 ################################################################################
 ### @brief propagate the locations into our programms:
 ################################################################################
@@ -151,7 +85,6 @@ to_native_path("CMAKE_INSTALL_FULL_LOCALSTATEDIR")
 to_native_path("CMAKE_INSTALL_FULL_SYSCONFDIR_SERENE")
 to_native_path("PKGDATADIR")
 to_native_path("CMAKE_INSTALL_DATAROOTDIR_SERENE")
-to_native_path("CMAKE_INSTALL_SBINDIR")
 to_native_path("CMAKE_INSTALL_BINDIR")
 to_native_path("INSTALL_ICU_DT_DEST")
 to_native_path("CMAKE_TEST_DIRECTORY")
@@ -165,50 +98,7 @@ configure_file(
 
 install(
     FILES "${CMAKE_SOURCE_DIR}/libs/basics/exitcodes.dat"
-    DESTINATION "${INSTALL_ICU_DT_DEST}"
-    RENAME exitcodes.dat
-)
-
-install(
-    FILES "${CMAKE_SOURCE_DIR}/packages/serenedb-helper"
-    DESTINATION "${INSTALL_ICU_DT_DEST}"
-    RENAME serenedb-helper
-)
-
-install(
-    FILES "${CMAKE_SOURCE_DIR}/packages/serenedb-update-db"
-    DESTINATION "${INSTALL_ICU_DT_DEST}"
-    RENAME serenedb-update-db
+    DESTINATION "${CMAKE_INSTALL_DOCDIR}"
 )
 
 install(FILES ${TZ_DATA_FILES} DESTINATION "${INSTALL_TZDATA_DEST}")
-
-if(THIRDPARTY_SBIN)
-    install(
-        FILES ${THIRDPARTY_SBIN}
-        PERMISSIONS
-            OWNER_READ
-            OWNER_WRITE
-            OWNER_EXECUTE
-            GROUP_READ
-            GROUP_EXECUTE
-            WORLD_READ
-            WORLD_EXECUTE
-        DESTINATION "${CMAKE_INSTALL_SBINDIR}"
-    )
-endif()
-
-if(THIRDPARTY_BIN)
-    install(
-        FILES ${THIRDPARTY_BIN}
-        PERMISSIONS
-            OWNER_READ
-            OWNER_WRITE
-            OWNER_EXECUTE
-            GROUP_READ
-            GROUP_EXECUTE
-            WORLD_READ
-            WORLD_EXECUTE
-        DESTINATION "${CMAKE_INSTALL_BINDIR}"
-    )
-endif()

93c3b5ac80339c4bd2b8243e42ecea443a3de4c9
Ivan Kochkarev <iakochkarev@edu.hse.ru>
2026-05-10 14:32:12 +0200

    Add ByTerms rule

--- libs/iresearch/include/iresearch/search/filter_rules.hpp
+++ libs/iresearch/include/iresearch/search/filter_rules.hpp
@@ -24,26 +24,28 @@
 #include <unordered_map>
 
 #include "iresearch/search/boolean_filter.hpp"
+#include "iresearch/search/term_filter.hpp"
+#include "iresearch/search/terms_filter.hpp"
 
 namespace irs {
-
 
 class FilterRulesConstructor {
  public:
   class FilterRule;
   using ptr = std::shared_ptr<FilterRule>;
-  class FilterRule { // think about managed_ptr like in Filter::Query
+  class FilterRule {  // think about managed_ptr like in Filter::Query
    public:
     using Base = FilterRule;
+    using TypesMap =
+      std::unordered_map<irs::TypeInfo::type_id, std::vector<irs::Filter::ptr>>;
     enum class FilterRuleTraversalType : uint8_t {
-      Bottom = 0, // dfs from the bottom to the top
-      Top = 1 // bfs from the top to the bottom
+      Bottom = 0,  // dfs from the bottom to the top
+      Top = 1      // bfs from the top to the bottom
     };
 
     FilterRule() = default;
     FilterRule(std::initializer_list<TypeInfo::type_id> filter_types)
-    : _desired_filter_types(std::move(filter_types))
-    {}
+      : _desired_filter_types(std::move(filter_types)) {}
 
     FilterRule& SetTraversalType(FilterRuleTraversalType type) {
       _traversal_type = type;
@@ -63,26 +65,35 @@ class FilterRulesConstructor {
     Filter::ptr ApplyWrapper(Filter::ptr filter) const;
 
     struct FilterRuleOptions {
-      bool has_other_filters = false; // for cases when current filter has filters which differ from desired_filter_types
+      bool has_other_filters =
+        false;  // for cases when current filter has filters which differ from
+                // desired_filter_types
     };
 
     struct FilterRuleResult {
-      Filter::ptr filter = nullptr; // return filter value - could be the same or new
-      bool is_new_filter = false; // signify that new filter was created
-      bool is_applied = false; // show if rule was applied
+      Filter::ptr filter =
+        nullptr;  // return filter value - could be the same or new
+      bool is_new_filter = false;  // signify that new filter was created
+      bool is_applied = false;     // show if rule was applied
     };
 
     // call of these functions depends on current_filter type
     // current_filter can be only AND, OR or NOT
-    inline virtual FilterRuleResult ApplyBoolean(And& current_filter, std::unordered_map<TypeInfo::type_id, std::vector<Filter::ptr>>&& sub_filters, FilterRuleOptions options) const {
+    inline virtual FilterRuleResult ApplyBoolean(
+      And& current_filter, TypesMap&& sub_filters,
+      FilterRuleOptions options) const {
       return FilterRuleResult{};
     };
 
-    inline virtual FilterRuleResult ApplyBoolean(Or& current_filter, std::unordered_map<TypeInfo::type_id, std::vector<Filter::ptr>>&& sub_filters, FilterRuleOptions options) const {
+    inline virtual FilterRuleResult ApplyBoolean(
+      Or& current_filter, TypesMap&& sub_filters,
+      FilterRuleOptions options) const {
       return FilterRuleResult{};
     };
 
-    inline virtual FilterRuleResult ApplyNot(Not& current_filter, Filter::ptr sub_filter, FilterRuleOptions options) const {
+    inline virtual FilterRuleResult ApplyNot(Not& current_filter,
+                                             Filter::ptr sub_filter,
+                                             FilterRuleOptions options) const {
       return FilterRuleResult{};
     };
 
@@ -92,14 +103,14 @@ class FilterRulesConstructor {
     // parameters like make traversal from top or bottom
     FilterRuleTraversalType _traversal_type = FilterRuleTraversalType::Bottom;
     std::set<TypeInfo::type_id> _desired_filter_types;
-    bool _works_with_empty_filters = false; // think about that, maybe want to have rule with zero suitable subfilters
+    bool _works_with_empty_filters =
+      false;  // think about that, maybe want to have rule with zero suitable
+              // subfilters
   };
 
   FilterRulesConstructor() = default;
 
-  void Add(ptr rule) {
-    _rules.push_back(std::move(rule));
-  }
+  void Add(ptr rule) { _rules.push_back(std::move(rule)); }
 
   template<typename FilterType, typename... Args>
   void Add(Args&&... args) {
@@ -109,7 +120,6 @@ class FilterRulesConstructor {
   Filter::ptr Apply(Filter::ptr filter) const;
 
  private:
-
   Filter::ptr AstTraversalFromBottom(Filter::ptr filter, ptr rule) const;
 
   std::vector<ptr> _rules;
@@ -120,21 +130,60 @@ class NotFilterRule : public FilterRulesConstructor::FilterRule {
   using FilterRule::ApplyBoolean;
   using FilterRule::ApplyNot;
 
-  NotFilterRule() : FilterRule({irs::Type<irs::Not>::id()}) 
-  {}
+  NotFilterRule() : FilterRule({irs::Type<irs::Not>::id()}) {}
 
   // think about boost in that context
-  virtual FilterRuleResult ApplyNot(Not& current_filter, Filter::ptr sub_filter, FilterRuleOptions options) const override {
-    if (sub_filter->type() != irs::Type<irs::Not>::id()) {
-      current_filter.assign(std::move(sub_filter));
-      return FilterRuleResult{};
-    }
-    auto& not_filter = sdb::basics::downCast<Not>(*sub_filter);
-    auto sub_sub_filter = not_filter.release();
-    return FilterRuleResult{.filter = std::move(sub_sub_filter), .is_new_filter = true};
-  }
+  virtual FilterRuleResult ApplyNot(Not& current_filter, Filter::ptr sub_filter,
+                                    FilterRuleOptions options) const override;
+
  private:
 };
 
+class AndFlatteningFilterRule : public FilterRulesConstructor::FilterRule {
+ public:
+  using FilterRule::ApplyBoolean;
+  using FilterRule::ApplyNot;
+
+  AndFlatteningFilterRule() : FilterRule({irs::Type<irs::And>::id()}) {}
+
+  // think about boost in that context
+  virtual FilterRuleResult ApplyBoolean(
+    And& current_filter, TypesMap&& sub_filters,
+    FilterRuleOptions options) const override;
+
+ private:
+};
+
+class OrFlatteningFilterRule : public FilterRulesConstructor::FilterRule {
+ public:
+  using FilterRule::ApplyBoolean;
+  using FilterRule::ApplyNot;
+
+  OrFlatteningFilterRule() : FilterRule({irs::Type<irs::Or>::id()}) {}
+
+  // think about boost in that context
+  virtual FilterRuleResult ApplyBoolean(
+    Or& current_filter, TypesMap&& sub_filters,
+    FilterRuleOptions options) const override;
+
+ private:
+};
+
+class ByTermsFilterRule : public FilterRulesConstructor::FilterRule {
+ public:
+  using FilterRule::ApplyBoolean;
+  using FilterRule::ApplyNot;
+
+  ByTermsFilterRule() : FilterRule({irs::Type<irs::ByTerm>::id()}) {}
+
+  // think about boost in that context
+  virtual FilterRuleResult ApplyBoolean(
+    And& current_filter, TypesMap&& sub_filters,
+    FilterRuleOptions options) const override;
+  // virtual FilterRuleResult ApplyBoolean(Or& current_filter, TypesMap&&
+  // sub_filters, FilterRuleOptions options) const override;
+
+ private:
+};
 
 }  // namespace irs
--- tests/libs/iresearch/search/filter_rules_tests.cpp
+++ tests/libs/iresearch/search/filter_rules_tests.cpp
@@ -49,6 +49,91 @@ namespace {
 
       ASSERT_EQ(root->type(), irs::Type<irs::Empty>::id());
     }
+
+    {
+      irs::FilterRulesConstructor constructor;
+
+      constructor.Add<irs::AndFlatteningFilterRule>();
+
+      irs::Filter::ptr root = std::make_unique<irs::And>();
+      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
+      auto& sub_and_filter = and_filter.add<irs::And>();
+      sub_and_filter.add<irs::Empty>();
+      sub_and_filter.add<irs::Empty>();
+      and_filter.add<irs::Empty>();
+      root = constructor.Apply(std::move(root));
+
+      auto& result = sdb::basics::downCast<irs::And>(*root);
+      ASSERT_EQ(result.size(), 3);
+      for (size_t i = 0; i < 3; ++i) {
+        ASSERT_EQ(result[i].type(), irs::Type<irs::Empty>::id());
+      }
+    }
+
+    {
+      irs::FilterRulesConstructor constructor;
+
+      constructor.Add<irs::OrFlatteningFilterRule>();
+
+      irs::Filter::ptr root = std::make_unique<irs::Or>();
+      auto& or_filter = sdb::basics::downCast<irs::Or>(*root);
+      auto& sub_or_filter = or_filter.add<irs::Or>();
+      sub_or_filter.add<irs::Empty>();
+      sub_or_filter.add<irs::Empty>();
+      or_filter.add<irs::Empty>();
+      root = constructor.Apply(std::move(root));
+
+      auto& result = sdb::basics::downCast<irs::Or>(*root);
+      ASSERT_EQ(result.size(), 3);
+      for (size_t i = 0; i < 3; ++i) {
+        ASSERT_EQ(or_filter[i].type(), irs::Type<irs::Empty>::id());
+      }
+    }
+
+    {
+      irs::FilterRulesConstructor constructor;
+
+      constructor.Add<irs::ByTermsFilterRule>();
+
+      irs::Filter::ptr root = std::make_unique<irs::And>();
+      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
+      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "cat");
+      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "dog");
+      root = constructor.Apply(std::move(root));
+
+      ASSERT_EQ(root->type(), irs::Type<irs::ByTerms>::id());
+      auto& by_terms = sdb::basics::downCast<irs::ByTerms>(*root);
+
+      const auto& field = by_terms.field();
+      ASSERT_EQ(field, "animal");
+
+      const auto& options = by_terms.options();
+      ASSERT_EQ(options.min_match, 2);
+      // ASSERT_EQ(options.terms.contains(""))
+
+    }
+
+    {
+      irs::FilterRulesConstructor constructor;
+
+      constructor.Add<irs::ByTermsFilterRule>();
+
+      irs::Filter::ptr root = std::make_unique<irs::And>();
+      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
+      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "cat");
+      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("birds", "dog");
+      root = constructor.Apply(std::move(root));
+
+      ASSERT_EQ(root->type(), irs::Type<irs::ByTerms>::id());
+      auto& by_terms = sdb::basics::downCast<irs::ByTerms>(*root);
+
+      const auto& field = by_terms.field();
+      ASSERT_EQ(field, "animal");
+
+      const auto& options = by_terms.options();
+      ASSERT_EQ(options.min_match, 2);
+      // ASSERT_EQ(options.terms.contains(""))
+    }
   }
 
   static constexpr auto kTestDirs = tests::GetDirectories<tests::kTypesDefault>();

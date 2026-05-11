55880a6eeff032de2b7a7b658a591d7633b98b73
Ivan Kochkarev <iakochkarev@edu.hse.ru>
2026-05-11 17:05:52 +0100

    Clang-format

--- tests/libs/iresearch/search/filter_rules_tests.cpp
+++ tests/libs/iresearch/search/filter_rules_tests.cpp
@@ -18,128 +18,104 @@
 /// Copyright holder is SereneDB GmbH, Berlin, Germany
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "iresearch/search/filter_rules.hpp"
-
 #include "filter_test_case_base.hpp"
+#include "iresearch/search/filter_rules.hpp"
 #include "tests_shared.hpp"
 
-
 namespace {
-  template<typename Filter>
-  Filter MakeFilter(const std::string_view& field, const std::string_view term) {
-    Filter q;
-    *q.mutable_field() = field;
-    q.mutable_options()->term = irs::ViewCast<irs::byte_type>(term);
-    return q;
+template<typename Filter>
+Filter MakeFilter(const std::string_view& field, const std::string_view term) {
+  Filter q;
+  *q.mutable_field() = field;
+  q.mutable_options()->term = irs::ViewCast<irs::byte_type>(term);
+  return q;
+}
+
+class FilterRuleTestCase : public tests::FilterTestCaseBase {};
+
+TEST_P(FilterRuleTestCase, test) {
+  {
+    irs::FilterRulesConstructor constructor;
+
+    constructor.Add<irs::NotFilterRule>();
+
+    irs::Filter::ptr root = std::make_unique<irs::Not>();
+    auto& not_filter = sdb::basics::downCast<irs::Not>(*root);
+    auto& sub_not_filter = not_filter.filter<irs::Not>();
+    sub_not_filter.filter<irs::Empty>();
+    root = constructor.Apply(std::move(root));
+
+    ASSERT_EQ(root->type(), irs::Type<irs::Empty>::id());
   }
 
-  class FilterRuleTestCase : public tests::FilterTestCaseBase {};
+  {
+    irs::FilterRulesConstructor constructor;
 
-  TEST_P(FilterRuleTestCase, test) {
-    {
-      irs::FilterRulesConstructor constructor;
+    constructor.Add<irs::AndFlatteningFilterRule>();
 
-      constructor.Add<irs::NotFilterRule>();
+    irs::Filter::ptr root = std::make_unique<irs::And>();
+    auto& and_filter = sdb::basics::downCast<irs::And>(*root);
+    auto& sub_and_filter = and_filter.add<irs::And>();
+    sub_and_filter.add<irs::Empty>();
+    sub_and_filter.add<irs::Empty>();
+    and_filter.add<irs::Empty>();
+    root = constructor.Apply(std::move(root));
 
-      irs::Filter::ptr root = std::make_unique<irs::Not>();
-      auto& not_filter = sdb::basics::downCast<irs::Not>(*root);
-      auto& sub_not_filter = not_filter.filter<irs::Not>();
-      sub_not_filter.filter<irs::Empty>();
-      root = constructor.Apply(std::move(root));
-
-      ASSERT_EQ(root->type(), irs::Type<irs::Empty>::id());
-    }
-
-    {
-      irs::FilterRulesConstructor constructor;
-
-      constructor.Add<irs::AndFlatteningFilterRule>();
-
-      irs::Filter::ptr root = std::make_unique<irs::And>();
-      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
-      auto& sub_and_filter = and_filter.add<irs::And>();
-      sub_and_filter.add<irs::Empty>();
-      sub_and_filter.add<irs::Empty>();
-      and_filter.add<irs::Empty>();
-      root = constructor.Apply(std::move(root));
-
-      auto& result = sdb::basics::downCast<irs::And>(*root);
-      ASSERT_EQ(result.size(), 3);
-      for (size_t i = 0; i < 3; ++i) {
-        ASSERT_EQ(result[i].type(), irs::Type<irs::Empty>::id());
-      }
-    }
-
-    {
-      irs::FilterRulesConstructor constructor;
-
-      constructor.Add<irs::OrFlatteningFilterRule>();
-
-      irs::Filter::ptr root = std::make_unique<irs::Or>();
-      auto& or_filter = sdb::basics::downCast<irs::Or>(*root);
-      auto& sub_or_filter = or_filter.add<irs::Or>();
-      sub_or_filter.add<irs::Empty>();
-      sub_or_filter.add<irs::Empty>();
-      or_filter.add<irs::Empty>();
-      root = constructor.Apply(std::move(root));
-
-      auto& result = sdb::basics::downCast<irs::Or>(*root);
-      ASSERT_EQ(result.size(), 3);
-      for (size_t i = 0; i < 3; ++i) {
-        ASSERT_EQ(or_filter[i].type(), irs::Type<irs::Empty>::id());
-      }
-    }
-
-    {
-      irs::FilterRulesConstructor constructor;
-
-      constructor.Add<irs::ByTermsFilterRule>();
-
-      irs::Filter::ptr root = std::make_unique<irs::And>();
-      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
-      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "cat");
-      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "dog");
-      root = constructor.Apply(std::move(root));
-
-      ASSERT_EQ(root->type(), irs::Type<irs::ByTerms>::id());
-      auto& by_terms = sdb::basics::downCast<irs::ByTerms>(*root);
-
-      const auto& field = by_terms.field();
-      ASSERT_EQ(field, "animal");
-
-      const auto& options = by_terms.options();
-      ASSERT_EQ(options.min_match, 2);
-      // ASSERT_EQ(options.terms.contains(""))
-
-    }
-
-    {
-      irs::FilterRulesConstructor constructor;
-
-      constructor.Add<irs::ByTermsFilterRule>();
-
-      irs::Filter::ptr root = std::make_unique<irs::And>();
-      auto& and_filter = sdb::basics::downCast<irs::And>(*root);
-      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "cat");
-      and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("birds", "dog");
-      root = constructor.Apply(std::move(root));
-
-      ASSERT_EQ(root->type(), irs::Type<irs::ByTerms>::id());
-      auto& by_terms = sdb::basics::downCast<irs::ByTerms>(*root);
-
-      const auto& field = by_terms.field();
-      ASSERT_EQ(field, "animal");
-
-      const auto& options = by_terms.options();
-      ASSERT_EQ(options.min_match, 2);
-      // ASSERT_EQ(options.terms.contains(""))
+    auto& result = sdb::basics::downCast<irs::And>(*root);
+    ASSERT_EQ(result.size(), 3);
+    for (size_t i = 0; i < 3; ++i) {
+      ASSERT_EQ(result[i].type(), irs::Type<irs::Empty>::id());
     }
   }
 
-  static constexpr auto kTestDirs = tests::GetDirectories<tests::kTypesDefault>();
+  {
+    irs::FilterRulesConstructor constructor;
 
-  INSTANTIATE_TEST_SUITE_P(rule_filter_test, FilterRuleTestCase,
+    constructor.Add<irs::OrFlatteningFilterRule>();
+
+    irs::Filter::ptr root = std::make_unique<irs::Or>();
+    auto& or_filter = sdb::basics::downCast<irs::Or>(*root);
+    auto& sub_or_filter = or_filter.add<irs::Or>();
+    sub_or_filter.add<irs::Empty>();
+    sub_or_filter.add<irs::Empty>();
+    or_filter.add<irs::Empty>();
+    root = constructor.Apply(std::move(root));
+
+    auto& result = sdb::basics::downCast<irs::Or>(*root);
+    ASSERT_EQ(result.size(), 3);
+    for (size_t i = 0; i < 3; ++i) {
+      ASSERT_EQ(or_filter[i].type(), irs::Type<irs::Empty>::id());
+    }
+  }
+
+  {
+    irs::FilterRulesConstructor constructor;
+
+    constructor.Add<irs::ByTermsFilterRule>();
+
+    irs::Filter::ptr root = std::make_unique<irs::And>();
+    auto& and_filter = sdb::basics::downCast<irs::And>(*root);
+    and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "cat");
+    and_filter.add<irs::ByTerm>() = MakeFilter<irs::ByTerm>("animal", "dog");
+    root = constructor.Apply(std::move(root));
+
+    ASSERT_EQ(root->type(), irs::Type<irs::ByTerms>::id());
+    auto& by_terms = sdb::basics::downCast<irs::ByTerms>(*root);
+
+    const auto& field = by_terms.field();
+    ASSERT_EQ(field, "animal");
+
+    const auto& options = by_terms.options();
+    ASSERT_EQ(options.min_match, 2);
+    // ASSERT_EQ(options.terms.contains(""))
+  }
+}
+
+static constexpr auto kTestDirs = tests::GetDirectories<tests::kTypesDefault>();
+
+INSTANTIATE_TEST_SUITE_P(rule_filter_test, FilterRuleTestCase,
                          ::testing::Combine(::testing::ValuesIn(kTestDirs),
                                             ::testing::Values("1_5simd")),
                          FilterRuleTestCase::to_string);
-}
+}  // namespace
+

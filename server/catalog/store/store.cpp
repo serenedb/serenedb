494b8c0d670fe2c689043f4b6a4ddd6c4dd82bf5
mbkkt <mbkkt@users.noreply.github.com>
2026-06-22 19:24:27 +0000

    refactor, review suggestion

--- server/catalog/store/store.cpp
+++ server/catalog/store/store.cpp
@@ -308,140 +308,242 @@ StoreTableDef MakeStoreTableDef(std::string_view database,
 void CatalogStore::WriteContext::PutDefinition(ObjectId parent_id,
                                                ObjectType type, ObjectId id,
                                                std::string_view def) {
-  _entries.push_back({.op = Op::PutDefinition,
-                      .key = {parent_id, type, id},
-                      .def = std::string{def}});
+  _entries.push_back({
+    .op = Op::PutDefinition,
+    .key =
+      {
+        parent_id,
+        type,
+        id,
+      },
+    .def = std::string{def},
+  });
 }
 
 void CatalogStore::WriteContext::PutSequence(ObjectId sequence_id,
                                              uint64_t value) {
-  _entries.push_back({.op = Op::PutSequence,
-                      .key = {.id = sequence_id},
-                      .sequence_value = value});
+  _entries.push_back({
+    .op = Op::PutSequence,
+    .key =
+      {
+        .id = sequence_id,
+      },
+    .sequence_value = value,
+  });
 }
 
 void CatalogStore::WriteContext::DropDefinition(ObjectId parent_id,
                                                 ObjectType type, ObjectId id) {
-  _entries.push_back({.op = Op::DropDefinition, .key = {parent_id, type, id}});
+  _entries.push_back({
+    .op = Op::DropDefinition,
+    .key =
+      {
+        .parent_id = parent_id,
+        .type = type,
+        .id = id,
+      },
+  });
 }
 
 void CatalogStore::WriteContext::DropSequence(ObjectId sequence_id) {
-  _entries.push_back({.op = Op::DropSequence, .key = {.id = sequence_id}});
+  _entries.push_back({
+    .op = Op::DropSequence,
+    .key =
+      {
+        .id = sequence_id,
+      },
+  });
 }
 
 void CatalogStore::WriteContext::CreateStoreTable(StoreTableDef def) {
-  _entries.push_back(
-    {.op = Op::CreateStoreTable, .store_table = std::move(def)});
+  _entries.push_back({
+    .op = Op::CreateStoreTable,
+    .store_table = std::move(def),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreTable(std::string name) {
-  _entries.push_back(
-    {.op = Op::DropStoreTable, .store_table = {.name = std::move(name)}});
+  _entries.push_back({
+    .op = Op::DropStoreTable,
+    .store_table =
+      {
+        .name = std::move(name),
+      },
+  });
 }
 
 void CatalogStore::WriteContext::RenameStoreTable(std::string name,
                                                   std::string new_name) {
-  _entries.push_back({.op = Op::RenameStoreTable,
-                      .store_table = {.name = std::move(name)},
-                      .name_a = std::move(new_name)});
+  _entries.push_back({
+    .op = Op::RenameStoreTable,
+    .store_table =
+      {
+        .name = std::move(name),
+      },
+    .name_a = std::move(new_name),
+  });
 }
 
 void CatalogStore::WriteContext::RenameStoreColumn(std::string table,
                                                    std::string name,
                                                    std::string new_name) {
-  _entries.push_back({.op = Op::RenameStoreColumn,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(name),
-                      .name_b = std::move(new_name)});
+  _entries.push_back({
+    .op = Op::RenameStoreColumn,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(name),
+    .name_b = std::move(new_name),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreColumn(std::string table,
                                                  std::string name) {
-  _entries.push_back({.op = Op::DropStoreColumn,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(name)});
+  _entries.push_back({
+    .op = Op::DropStoreColumn,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(name),
+  });
 }
 
 void CatalogStore::WriteContext::AddStoreColumn(std::string table,
                                                 std::string name,
                                                 std::string type_sql,
                                                 std::string default_sql) {
-  _entries.push_back({.op = Op::AddStoreColumn,
-                      .def = std::move(default_sql),
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(name),
-                      .name_b = std::move(type_sql)});
+  _entries.push_back({
+    .op = Op::AddStoreColumn,
+    .def = std::move(default_sql),
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(name),
+    .name_b = std::move(type_sql),
+  });
 }
 
 void CatalogStore::WriteContext::ChangeStoreColumnType(std::string table,
                                                        std::string name,
                                                        std::string type_sql,
                                                        std::string using_sql) {
-  _entries.push_back({.op = Op::ChangeStoreColumnType,
-                      .def = std::move(using_sql),
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(name),
-                      .name_b = std::move(type_sql)});
+  _entries.push_back({
+    .op = Op::ChangeStoreColumnType,
+    .def = std::move(using_sql),
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(name),
+    .name_b = std::move(type_sql),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreForeignKey(std::string table,
                                                      std::string other) {
-  _entries.push_back({.op = Op::DropStoreForeignKey,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(other)});
+  _entries.push_back({
+    .op = Op::DropStoreForeignKey,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(other),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreCheck(std::string table,
                                                 std::string expr) {
-  _entries.push_back({.op = Op::DropStoreCheck,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(expr)});
+  _entries.push_back({
+    .op = Op::DropStoreCheck,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(expr),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreNotNull(std::string table,
                                                   std::string column) {
-  _entries.push_back({.op = Op::DropStoreNotNull,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(column)});
+  _entries.push_back({
+    .op = Op::DropStoreNotNull,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(column),
+  });
 }
 
 void CatalogStore::WriteContext::AddStoreNotNull(std::string table,
                                                  std::string column) {
-  _entries.push_back({.op = Op::AddStoreNotNull,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(column)});
+  _entries.push_back({
+    .op = Op::AddStoreNotNull,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(column),
+  });
 }
 
 void CatalogStore::WriteContext::AddStoreCheck(std::string table,
                                                std::string expr) {
-  _entries.push_back({.op = Op::AddStoreCheck,
-                      .store_table = {.name = std::move(table)},
-                      .name_a = std::move(expr)});
+  _entries.push_back({
+    .op = Op::AddStoreCheck,
+    .store_table =
+      {
+        .name = std::move(table),
+      },
+    .name_a = std::move(expr),
+  });
 }
 
 void CatalogStore::WriteContext::AddStorePrimaryKey(
   std::string table, std::vector<std::string> columns) {
-  _entries.push_back({.op = Op::AddStorePrimaryKey,
-                      .store_table = {.name = std::move(table),
-                                      .pk_columns = std::move(columns)}});
+  _entries.push_back({
+    .op = Op::AddStorePrimaryKey,
+    .store_table =
+      {
+        .name = std::move(table),
+        .pk_columns = std::move(columns),
+      },
+  });
 }
 
 void CatalogStore::WriteContext::AddStoreUnique(
   std::string table, std::vector<std::string> columns) {
-  _entries.push_back(
-    {.op = Op::AddStoreUnique,
-     .store_table = {.name = std::move(table),
-                     .unique_constraints = {std::move(columns)}}});
+  _entries.push_back({
+    .op = Op::AddStoreUnique,
+    .store_table =
+      {
+        .name = std::move(table),
+        .unique_constraints =
+          {
+            std::move(columns),
+          },
+      },
+  });
 }
 
 void CatalogStore::WriteContext::CreateStoreIndex(StoreIndexDef def) {
-  _entries.push_back(
-    {.op = Op::CreateStoreIndex, .store_index = std::move(def)});
+  _entries.push_back({
+    .op = Op::CreateStoreIndex,
+    .store_index = std::move(def),
+  });
 }
 
 void CatalogStore::WriteContext::DropStoreIndex(ObjectId index_id) {
-  _entries.push_back(
-    {.op = Op::DropStoreIndex, .store_index = {.index_id = index_id}});
+  _entries.push_back({
+    .op = Op::DropStoreIndex,
+    .store_index =
+      {
+        .index_id = index_id,
+      },
+  });
 }
 
 void CatalogStore::WriteContext::WriteTombstone(ObjectId parent_id,

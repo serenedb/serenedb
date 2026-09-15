fab070154fbe97d93b830508c2532d37c7baab73
Andrei L <andrei.lobov@serenedb.com>
2026-09-15 11:52:43 +0200

    add latest

--- scripts/perf/bench_remote_fs.sh
+++ scripts/perf/bench_remote_fs.sh
@@ -76,7 +76,7 @@ EOF
 : "${BRFS_KEEP:=1}"
 : "${BRFS_BUCKET:=testbucket}"
 : "${BRFS_CONTAINER:=testcont}"
-: "${BRFS_MINIO_IMAGE:=pgsty/minio}"
+: "${BRFS_MINIO_IMAGE:=pgsty/minio:latest}"
 : "${BRFS_AZURITE_IMAGE:=mcr.microsoft.com/azure-storage/azurite}"
 : "${BRFS_TOXIPROXY_IMAGE:=ghcr.io/shopify/toxiproxy}"
 : "${BRFS_NETSHOOT_IMAGE:=nicolaka/netshoot}"
